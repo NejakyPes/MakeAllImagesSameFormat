7abd3f0af0981ed6f8e1509223c23e580c50d504
NejakyPes <kernolog@gmail.com>
2020-07-11 11:44:19 +0200

    Added logs

--- MakeAllImagesSameFormat/Program.cs
+++ MakeAllImagesSameFormat/Program.cs
@@ -13,36 +13,45 @@ namespace MakeAllImagesSameFormat
     {
         static void Main(string[] args)
         {
-            foreach (string file in Directory.EnumerateFiles(Variables.FileLocation))
+            Log("Application started");
+            IEnumerable<string> files = Directory.EnumerateFiles(Variables.FileLocation);
+            if (files.Count() > 0)
             {
-                try
+                Log($"Founded {files.Count()} files to process");
+                foreach (string file in files)
                 {
-                    string[] fileParse = file.Split('\\');
-                    string name = fileParse[fileParse.Length - 1];
-
-                    Regex regex = new Regex(@"\d{8}_\d{6}(\.jpg|\.png)");
-                    Match match = regex.Match(name);
-                    if (match.Success)
+                    try
                     {
-                        string folder = $"{ Variables.FileLocation}\\{ match.Value.Split('_')[0]}";
-                        Directory.CreateDirectory(folder);
-                        if (Directory.EnumerateFiles(folder).Any(s => s.Contains(match.Value)))
+                        string[] fileParse = file.Split('\\');
+                        string name = fileParse[fileParse.Length - 1];
+
+                        Regex regex = new Regex(@"\d{8}_\d{6}(\.jpg|\.png)");
+                        Match match = regex.Match(name);
+                        if (match.Success)
                         {
-                            Delete("Duplicate",name);
-                            continue;
+                            string folder = $"{ Variables.FileLocation}\\{ match.Value.Split('_')[0]}";
+                            Directory.CreateDirectory(folder);
+                            if (Directory.EnumerateFiles(folder).Any(s => s.Contains(match.Value)))
+                            {
+                                Delete("Duplicate", name);
+                                continue;
+                            }
+                            //Console.WriteLine($"{name} => {Variables.FileLocation}\\{match.Value}");
+                            Log($"Move[{name}] | {Variables.FileLocation}\\{folder}\\{match.Value}");
+                            File.Move($"{Variables.FileLocation}\\{name}", $"{Variables.FileLocation}\\{folder}\\{match.Value}");
                         }
-                        //Console.WriteLine($"{name} => {Variables.FileLocation}\\{match.Value}");
-                        Log($"Move[{name}] | {Variables.FileLocation}\\{folder}\\{match.Value}");
-                        File.Move($"{Variables.FileLocation}\\{name}", $"{Variables.FileLocation}\\{folder}\\{match.Value}");
+                        else
+                            Delete("Regex.NotMatch", name);
                     }
-                    else
-                        Delete("Regex.NotMatch",name);
-                }
-                catch (Exception e)
-                {
-                    Log($"Error | {e.Message}");
+                    catch (Exception e)
+                    {
+                        Log($"Error | {e.Message}");
+                    }
                 }
             }
+            else
+                Log("None file found");
+            Log("Application ended");
         }
 
         private static void Delete(string reason, string name)
